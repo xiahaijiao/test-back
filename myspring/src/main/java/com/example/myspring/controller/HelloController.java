4fa30d6351d0278864f702bd1d94b26039a1f947
haijiao.xia <haijiao.xia@h-shgroup.com>
2023-03-15 19:12:42 +0800

    创建项目

---
+++ myspring/src/main/java/com/example/myspring/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.myspring.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(){
+        System.out.println("hello world");
+        return "hello world";
+    }
+}
